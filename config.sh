141b90f4b435e96ad7e9d4eaca38e49b6c767862
aldofalso <aldo.falso@outlook.com>
2020-10-29 11:20:47 +0100

    add rpm

--- config.sh
+++ config.sh
@@ -15,6 +15,9 @@ sudo dnf install zsh gnome-tweaks ffmpeg gimp cmatrix lm_sensors neovim youtube-
 #add flathub repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+#add rpm repo
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 #dots
 cp -r ~/dots/nvim ~/.config
 cp -r ~/dots/.zshrc ~/
