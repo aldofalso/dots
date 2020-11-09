bae17206be77c5776551ec69bd596723a37f6f6f
aldofalso <aldo.falso@outlook.com>
2020-11-09 19:04:25 +0100

    switch to ungoogled-chromium

--- config.sh
+++ config.sh
@@ -7,19 +7,19 @@ echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
 sudo systemctl disable NetworkManager-wait-online.service
 
 #remove packages 
-sudo dnf remove rhythmbox gnome-maps gnome-contacts cheese nano firefox 
+sudo dnf -y remove rhythmbox gnome-maps gnome-contacts cheese nano firefox 
 
 #add flathub repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #add rpm repo
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -f install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #dnf update
-sudo dnf update
+sudo dnf -y update
 
 #install packages
-sudo dnf install zsh gnome-tweaks ffmpeg gimp cmatrix lm_sensors vim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-freeworld telegram-desktop
+sudo dnf -y install zsh gnome-tweaks ffmpeg gimp cmatrix lm_sensors vim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-browser-privacy telegram-desktop
 
 #dots
 cp -r ~/dots/.vimrc ~/
