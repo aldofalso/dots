137b8abe8c8118a7451344eb7a973ed00a8d178d
aldofalso <aldo.falso@outlook.com>
2020-12-06 13:08:58 +0100

    new packages and new wall

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
 sudo systemctl disable NetworkManager-wait-online.service
 
 #remove packages 
-sudo dnf -y remove rhythmbox gnome-maps gnome-contacts cheese nano firefox fedora-chromium-config 
+sudo dnf -y remove rhythmbox gnome-maps gnome-contacts nano firefox fedora-chromium-config gnome-weather
 
 #add flathub repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -15,11 +15,15 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 #add rpm repo
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+#add vscode repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 #dnf update
 sudo dnf -y update
 
 #install packages
-sudo dnf -y install zsh gnome-tweaks ffmpeg gimp cmatrix lm_sensors neovim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-browser-privacy telegram-desktop
+sudo dnf -y install zsh gnome-tweaks ffmpeg gimp cmatrix lm_sensors neovim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-browser-privacy chromium-freeworld telegram-desktop code
 
 #dots
 cp -r ~/dots/nvim ~/.config
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,4 +1,3 @@
-
 if ! filereadable(system('echo -n "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/autoload/plug.vim"'))
 	echo "Downloading plugins..."
 	silent !mkdir -p ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/autoload/
@@ -11,7 +10,6 @@ endif
 call plug#begin(system('echo -n "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/plugged"'))
 
       Plug 'bling/vim-airline'
-      Plug 'morhetz/gruvbox'
       Plug 'jiangmiao/auto-pairs'
       Plug 'ap/vim-css-color'
 
@@ -22,7 +20,6 @@ call plug#end()
 let g:airline_powerline_fonts = 1
 set guicursor+=a:blinkon1
 set cursorline
-set termguicolors
 set encoding=utf-8
 set number
 set mouse=a
@@ -33,5 +30,4 @@ set smartindent
 set autoindent
 set fileformat=unix
 set noshowmode
-colorscheme gruvbox
 let g:airline#extensions#whitespace#enabled = 0 
