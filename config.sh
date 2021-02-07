eb52c032fe82f454b49f40eafda5625eb5abae2c
aldofalso <aldo.falso@outlook.com>
2021-02-07 17:26:11 +0100

    update

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
 sudo systemctl disable NetworkManager-wait-online.service
 
 # remove packages 
-sudo dnf -y remove rhythmbox gnome-maps gnome-contacts nano firefox fedora-chromium-config gnome-weather abrt virtualbox-guest-additions gnome-software
+sudo dnf -y remove rhythmbox gnome-maps gnome-contacts nano firefox fedora-chromium-config gnome-weather abrt virtualbox-guest-additions gnome-software totem
 
 # add flathub repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -23,7 +23,7 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 sudo dnf -y update
 
 # install packages
-sudo dnf -y install gnome-tweaks ffmpeg gimp cmatrix cava lm_sensors vim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-browser-privacy chromium-freeworld telegram-desktop code neofetch transmission levien-inconsolata-fonts
+sudo dnf -y install gnome-tweaks ffmpeg gimp cmatrix cava lm_sensors vim youtube-dl python3-speedtest-cli gcc kernel-devel make g++ chromium-browser-privacy chromium-freeworld telegram-desktop code neofetch transmission levien-inconsolata-fonts vlc
 
 # dots
 cp -r ~/dots/.vimrc ~/
@@ -32,6 +32,11 @@ sudo cp ~/dots/scripts/* /usr/local/bin
 
 # gnome settings
 rm -rf ~/.config/dconf
-cp -r ~/dots/dconf ~/.config 
+cp -r ~/dots/dconf ~/.config
+
+# if laptop set hostname
+if [ -d /sys/class/power_supply/BAT0* ]; then
+  hostnamectl set-hostname thinkpad
+fi
 
 echo done
